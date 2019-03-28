892ea57259ac75059a5e62afbb1175602536bc9c
Francisco Luongo <fluongo@gmail.com>
2019-03-28 16:44:29 -0700

    updated raster plot with bokeh

--- scrapc_viz.py
+++ scrapc_viz.py
@@ -12,6 +12,10 @@ from scipy.ndimage import gaussian_filter
 from scipy.ndimage.filters import gaussian_filter1d
 from matplotlib.colors import Normalize
 import seaborn as sns
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.layouts import row, gridplot
+from bokeh.plotting import figure, show, output_file
+
 
 # Visualization arm of SCRAPC
 
@@ -32,14 +36,35 @@ def plot_image_with_alpha(im, mask, thresh = 0.4, vmin = -np.pi, vmax = np.pi):
     plt.imshow(np.ones_like(im), vmin = 0, vmax = 1, cmap= 'binary')
     plt.imshow(colors)
 
-def plot_raster(st, cl, cluster_numbers = range(10), trange = [5000., 5050.], labels = None):
+def plot_raster(st, cl, cluster_numbers = range(10), trange = [5000., 5050.], labels = None, use_bokeh = False):
     ''' Plots a raster of the given cluster numbers in the given range trange'''
+    
+    if labels != None:
+        assert len(cluster_numbers) == len(labels)
+    
     lo, hi = trange
     colors = sns.color_palette("hls", len(cluster_numbers))
+    if use_bokeh:
+        TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select"
+        p = figure(title="Raster", tools=TOOLS, width = 1000, height = 300)
+        p.background_fill_color = "black"
+
     for neuron_id, cc in enumerate(cluster_numbers):
         tmp = st[cl==cc]
         sub = tmp[(tmp>=lo)*(tmp<=hi)]
         current_label = str(cc) if labels == None else labels[neuron_id]
-        plt.eventplot(sub, lineoffsets=neuron_id, colors = colors[neuron_id], label = current_label) 
+        if use_bokeh == True:
+            # Make a list of lists for each spike and add as a tick mark
+            xs = [[i, i] for i in sub]
+            ys = [[neuron_id, neuron_id+0.75] for i in sub]
+            p.multi_line(xs, ys, line_color = colors.as_hex()[neuron_id], legend = current_label)
+            
+        else: # Use matplotlib
+            plt.eventplot(sub, lineoffsets=neuron_id, colors = colors[neuron_id], 
+            label = current_label) 
         #print(len(sub), end=" ")
 
+    if use_bokeh:
+        show(p)
+    else:
+        plt.legend()
